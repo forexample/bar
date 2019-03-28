520cef31da0c8f025557ec61d84ffcfb06c1c63d
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-03-29 00:58:30 +0300

    Init

---
+++ bar/bar.cpp
@@ -0,0 +1,13 @@
+#include <bar/bar.hpp>
+
+#include <iostream> // std::cout
+
+namespace bar
+{
+
+void bar::Run()
+{
+  std::cout << "bar::Run" << std::endl;
+}
+
+} // namespace bar
---
+++ bar/bar.hpp
@@ -0,0 +1,15 @@
+#ifndef BAR_BAR_HPP_
+#define BAR_BAR_HPP_
+
+namespace bar
+{
+
+class bar
+{
+ public:
+  void Run();
+};
+
+} // namespace bar
+
+#endif // BAR_BAR_HPP_
